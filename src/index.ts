878034940273e3cd0048355a7450f16ab62c5a3d
klimovich008 <klimovich008@gmail.com>
2025-06-08 23:08:09 +0200

    Every 5 min fix

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import { launchBot } from "./bot";
 
 launchBot();
 
-cron.schedule("* 5 * * *", () => {
+cron.schedule("*/5 * * * *", () => {
 	console.log("Running cron job to update apostile info...");
 	updateApostileInfo(20);
 });
